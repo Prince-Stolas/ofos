a7033c4f7de734431d18476e9c09c16f1398506d
Stolas <valentin.sieber7d@web.de>
2023-06-18 21:37:50 +0200

    passt daweil, ich mach morgen weiter

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ let ctx = canv.getContext("2d")
 canv.innerWidth = window.innerWidth
 canv.innerHeight = window.innerHeigh
 
-ctx.fillStyle = "#ff0000"
-ctx.fillRect(0, 0, 100, 100)
+//ctx.fillStyle = "#ff0000"
+//ctx.fillRect(0, 0, 100, 100)
+
+class rechteck {
+    erstellen() {
+        console.log("test")
+    }
+}
+
+let x = new rechteck
+x.erstellen()
