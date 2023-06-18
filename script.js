19e5f8015f251530a8f3e6f95a881babbc2ae4f5
Stolas <valentin.sieber7d@web.de>
2023-06-18 21:34:08 +0200

    first simple setup

---
+++ script.js
@@ -0,0 +1,8 @@
+const canv = document.getElementById("_canvas")
+let ctx = canv.getContext("2d")
+
+canv.innerWidth = window.innerWidth
+canv.innerHeight = window.innerHeigh
+
+ctx.fillStyle = "#ff0000"
+ctx.fillRect(0, 0, 100, 100)
